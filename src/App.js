0b0c6d333b91479c76f428c1a5929b013bffc99f
Margarita Shishkova <margolen239@gmail.com>
2023-02-15 21:40:39 +0000

    Create component Home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
 import words from "./words.json";
 import FlashCard from "./components/FlashCard/FlashCard";
 
@@ -9,6 +10,7 @@ export default function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
       {words.map((word) => {
         return <FlashCard {...word} />;
       })}
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import CSSModules from "react-css-modules";
+import styles from "./style.module.scss";
+
+function Home() {
+  return (
+    <div styleName="home">
+    
+    <img src="img/flag.png" alt="flag of the UK" />
+    <button>Get started</button>
+    
+
+    </div>
+  );
+}
+
+export default CSSModules(Home, styles);
