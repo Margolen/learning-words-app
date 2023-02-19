9799c0eff61ff0620f5d8a051e327be51e5e6edf
Margarita Shishkova <margolen239@gmail.com>
2023-02-19 19:36:57 +0000

    New state for editing

--- src/components/TableRow/TableRow.jsx
+++ src/components/TableRow/TableRow.jsx
@@ -1,16 +1,33 @@
+import { useState } from 'react';
 import CSSModules from 'react-css-modules';
 import styles from './style.module.scss';
 
 function TableRow(props) {
   const { english, transcription, russian } = props.word;
+  const [edited, setEdited] = useState(false);
+
   return (
     <tr>
       <td>{english}</td>
       <td>{transcription}</td>
       <td>{russian}</td>
       <td>
-        <img styleName="img-row" src="./img/close.png" alt="" />
-        <img styleName="img-row" src="./img/edit.png" alt="" />
+        {edited ? (
+          <img
+            onClick={() => setEdited(false)}
+            styleName="img-row"
+            src="./img/cancel.png"
+            alt=""
+          />
+        ) : (
+          <></>
+        )}
+        <img
+          onClick={() => setEdited(true)}
+          styleName="img-row"
+          src="./img/edit.png"
+          alt=""
+        />
         <img styleName="img-row" src="./img/delete.png" alt="" />
       </td>
     </tr>
