1c749f2188e686ef9dda5a7e88c68b782a019850
Margarita Shishkova <margolen239@gmail.com>
2023-03-25 20:22:09 +0000

    Create PageError

--- src/pages/PageError/PageError.jsx
+++ src/pages/PageError/PageError.jsx
@@ -1,5 +1,17 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import CSSModules from 'react-css-modules';
+import styles from './style.module.scss';
 
-export default function PageError() {
-  return <div>PageError</div>;
+function PageError() {
+  const navigate = useNavigate();
+  return (
+    <div styleName="container">
+      <h1>Page Error </h1>
+      <button onClick={() => navigate('')} styleName="link_404">
+        Go to Home
+      </button>
+    </div>
+  );
 }
+
+export default CSSModules(PageError, styles);
